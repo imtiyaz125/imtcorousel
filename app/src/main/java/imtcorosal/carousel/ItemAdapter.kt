1f8f0355a9f1ee523ccc723e2c34209244f01ea3
imtiyaz <imtiyaz@techaheadcorp.com>
2019-08-08 11:57:44 +0530

    first full push

---
+++ app/src/main/java/imtcorosal/carousel/ItemAdapter.kt
@@ -0,0 +1,36 @@
+package imtcorosal.carousel
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.list_item.view.*
+
+class ItemAdapter(val itemClick: (position:Int,item:Item) -> Unit) : RecyclerView.Adapter<ItemViewHolder>() {
+
+    private var items: List<Item> = listOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder =
+        ItemViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false))
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.bind(items[position])
+        holder.itemView.setOnClickListener {
+            itemClick(position,items[position])
+        }
+    }
+
+    override fun getItemCount() = items.size
+
+    fun setItems(newItems: List<Item>) {
+        items = newItems
+        notifyDataSetChanged()
+    }
+}
+
+class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+    fun bind(item: Item) {
+        view.list_item_text.text = "${item.title}"
+        view.list_item_icon.setImageResource(item.icon)
+    }
+}
---
+++ app/src/main/java/imtcorosal/carousel/MainActivity.kt
@@ -0,0 +1,43 @@
+package imtcorosal.carousel
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.DrawableRes
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val itemAdapter by lazy {
+        ItemAdapter { position: Int, item: Item ->
+            Toast.makeText(this@MainActivity, "Pos ${position}", Toast.LENGTH_LONG).show()
+            item_list.smoothScrollToPosition(position)
+        } }
+    private val possibleItems = listOf(
+        Item("Airplanes", R.drawable.ic_airplane),
+        Item("Cars", R.drawable.ic_car),
+        Item("Food", R.drawable.ic_food),
+        Item("Gas", R.drawable.ic_gas),
+        Item("Home", R.drawable.ic_home)
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        item_list.initialize(itemAdapter)
+        item_list.setViewsToChangeColor(listOf(R.id.list_item_background, R.id.list_item_text))
+        itemAdapter.setItems(getLargeListOfItems())
+    }
+
+    private fun getLargeListOfItems(): List<Item> {
+        val items = mutableListOf<Item>()
+        (0..40).map { items.add(possibleItems.random()) }
+        return items
+    }
+}
+
+data class Item(
+    val title: String,
+    @DrawableRes val icon: Int
+)
